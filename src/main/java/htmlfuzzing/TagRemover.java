c6fc779a7fe49e92feaa74c2c3bd43b0481d16bb
aclash <aclash@163.com>
2018-09-15 18:23:27 -0700

    randomly remove certain tag which can be <XXX> or </XXX>

--- src/main/java/htmlfuzzing/TagRemover.java
+++ src/main/java/htmlfuzzing/TagRemover.java
@@ -7,19 +7,19 @@ import java.util.Random;
 
 public class TagRemover implements Fuzzer{
     @Override
-    public String fuzz(String str){                         //randomly pick a tag and remove it
+    public String fuzz(String str){                             //randomly pick a tag and remove it
         ArrayList<Integer> list = new ArrayList<>();
         String regex = "(<[^>]+>)|(<[^>]+/>)";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(str);
         while(m.find()) {
-            list.add(m.start());                            //find all the tag and put into arraylist
+            list.add(m.start());                                //find all the tag and put into arraylist
         }
         Random rand = new Random();
-        int rdIndex = rand.nextInt(list.size());            //randomly pick a tag could be <xxx> or <xxx/>
+        int rdIndex = rand.nextInt(list.size());                //randomly pick a tag could be <xxx> or <xxx/>
         String res1 = str.substring(0, list.get(rdIndex));
         String res2 = str.substring(list.get(rdIndex));
-        res2 = res2.replaceFirst("(<[^>]+>)|(<[^>]+/>)",""); //remove the first tag
+        res2 = res2.replaceFirst("(<[^>]+>)|(<[^>]+/>)",""); //remove the start tag or close tag
         return res1 + res2;
     }
 }
--- src/main/java/htmlfuzzing/TagReplacer.java
+++ src/main/java/htmlfuzzing/TagReplacer.java
@@ -1,9 +1,26 @@
 package htmlfuzzing;
 import htmlfuzzing.spi.Fuzzer;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TagReplacer implements Fuzzer{
     @Override
     public String fuzz(String str){
-        String res = str.replaceFirst("<[^>]+>","<script>"); //repalce the first tag to <script>
-        return res;
+        ArrayList<Integer> list = new ArrayList<>();
+        String regex = "(<[^>]+>)|(<[^>]+/>)";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(str);
+        while(m.find()) {
+            list.add(m.start());                                //find all the tag and put into arraylist
+        }
+        Random rand = new Random();
+        int rdIndex = rand.nextInt(list.size());                //randomly pick a tag could be <xxx> or <xxx/>
+        String res1 = str.substring(0, list.get(rdIndex));
+        String res2 = str.substring(list.get(rdIndex));
+        res2 = res2.replaceFirst("(<[^>]+>)|(<[^>]+/>)","</script>"); //remove the start tag or close tag
+        return res1 + res2;
     }
 }
